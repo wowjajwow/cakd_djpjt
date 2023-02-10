8895abacaceadc6634842689935f72c01c0228d9
wowjajwow <wjdgh945@naver.com>
2023-02-10 11:25:54 +0900

    static 정적파일, css link, landing, about_me, 카테고리 기능추가

--- blog/admin.py
+++ blog/admin.py
@@ -1,14 +1,13 @@
 from django.contrib import admin
-from .models import Post
 # from markdownx.admin import MarkdownxModelAdmin
-# from .models import Post, Category, Tag, Comment
+from .models import Post, Category
 
 admin.site.register(Post)
 
-# class CategoryAdmin(admin.ModelAdmin):
-#     prepopulated_fields = {'slug' : ('name', )}
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug' : ('name', )}
 
-# admin.site.register(Category, CategoryAdmin)
+admin.site.register(Category, CategoryAdmin)
 
 # class TagAdmin(admin.ModelAdmin):
 #     prepopulated_fields = {'slug' : ('name', )}
--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('<int:pk>/', views.PostDetail.as_view()),
     path('', views.PostList.as_view()),
+    path('category/<str:slug>', views.category_page), 
 ]
